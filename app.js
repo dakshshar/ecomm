ce47615902e851bb341877327cc7495e7583d53c
dakshshar <85700619+dakshshar@users.noreply.github.com>
2025-05-25 17:46:43 +0530

    p

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   dbName: process.env.DB_NAME,
 })
   .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.error('❌ MongoDB connection e rror:', err));
+  .catch(err => console.error('❌ MongoDB conn ection e rror:', err));
 
 // API Routes
 app.use(`${process.env.API_URL}/users`, require('./routes/user'));
